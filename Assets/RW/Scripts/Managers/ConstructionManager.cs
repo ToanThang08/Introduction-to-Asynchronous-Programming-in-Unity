72309caa38098e450f5ef5e561ff23b2339d4a80
Toan Thang <box.dota@gmail.com>
2023-08-22 18:57:57 +0300

    Adding await to Start Running Asynchronous Code

--- Assets/RW/Scripts/Managers/ConstructionManager.cs
+++ Assets/RW/Scripts/Managers/ConstructionManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -9,37 +10,34 @@ namespace RayWenderlich.WenderlichTopia
         public GameObject constructionTilePrefab;
         public UiManager uiManager;
         public Transform levelGeometryContainer;
-        
+
         private void Start()
         {
-            
-        }
 
-        public void BuildStructure(GameObject placementStructure, Vector3 buildPosition)
-        {
-            if (placementStructure.TryGetComponent(out RoadBuildPropertiesContainer roadBuildPropertiesContainer))
-            {
-                Destroy(placementStructure);
-                var roadProperties = roadBuildPropertiesContainer.roadBuildProperties;
-
-            }
-            else if (placementStructure.TryGetComponent(out HouseBuildPropertiesContainer houseBuildPropertiesContainer))
-            {
-                Destroy(placementStructure); 
-                var houseProperties = houseBuildPropertiesContainer.houseBuildProperties;
-                
-            }
         }
 
         public async void BuildStructure(GameObject placementStructure, Vector3 buildPosition)
         {
-            
+            // Method code goes here
+            var buildRoadTask = BuildRoadAsync(roadProperties, buildPosition);
+            await buildRoadTask;
+            uiManager.NewStructureComplete(roadProperties.roadCost, buildPosition);
+
+        }
+
+        private async Task BuildRoadAsync(RoadBuildProperties roadProperties, Vector3 buildPosition)
+        {
+            var constructionTile = Instantiate(constructionTilePrefab, buildPosition, Quaternion.identity, levelGeometryContainer);
+            await Task.Delay(2500);
+            Destroy(constructionTile);
+            Instantiate(roadProperties.completedRoadPrefab, buildPosition, Quaternion.identity, levelGeometryContainer);
+
         }
 
 
         private void Update()
         {
-            
+
         }
 
         private void OnDisable()
@@ -48,4 +46,3 @@ namespace RayWenderlich.WenderlichTopia
         }
     }
 }
-
